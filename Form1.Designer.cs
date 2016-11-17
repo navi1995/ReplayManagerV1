9ec811dde6e1918c0d86dce1e9320fada1dd0505
navi1995 <navi1995@hotmail.com>
2016-11-18 01:30:41 +1300

    Added project
    Added project

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ReplayManagerv1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(360, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please add directories through the settings menu!";
+            // 
+            // listView1
+            // 
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(30, 82);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(926, 337);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 526);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "League Replay Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReplayManagerv1
+{
+    public partial class Form1 : Form
+    {
+        private FolderBrowserDialog folderBrowserDialog1;
+        
+
+        private MainMenu mainMenu1;
+        private MenuItem fileMenuItem, leagueDirectoryMenuItem, replayDirectoryMenuItem;
+
+        private string leagueDirectory = @"D:\Riot Games\League of Legends\RADS\solutions\lol_game_client_sln\releases\0.0.1.152\deploy\League of Legends.exe";
+        private string replayDirectory = @"D:\Users\Navi Jador\Documents\League of Legends\Replays\";
+
+        private bool fileOpened = false;
+
+        public Form1()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.fileMenuItem = new System.Windows.Forms.MenuItem();
+            this.leagueDirectoryMenuItem = new System.Windows.Forms.MenuItem();
+            this.replayDirectoryMenuItem = new System.Windows.Forms.MenuItem();
+            
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+
+            this.mainMenu1.MenuItems.Add(this.fileMenuItem);
+            this.fileMenuItem.MenuItems.AddRange(
+                                new System.Windows.Forms.MenuItem[] {this.leagueDirectoryMenuItem,
+                                                                 this.replayDirectoryMenuItem});
+            this.fileMenuItem.Text = "Settings";
+
+            this.leagueDirectoryMenuItem.Text = "Set League Directory...";
+            this.leagueDirectoryMenuItem.Click += new System.EventHandler(this.leagueDirectoryMenuItem_Click);
+
+            this.replayDirectoryMenuItem.Text = "Set Replay Directory...";
+            this.replayDirectoryMenuItem.Click += new System.EventHandler(this.replayDirectoryMenuItem_Click);
+
+            // Set the help text description for the FolderBrowserDialog.
+            this.folderBrowserDialog1.Description =
+                "Select the directory that you want to use as the default.";
+
+            // Do not allow the user to create new files via the FolderBrowserDialog.
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+
+            // Default to the My Documents folder.
+
+            this.ClientSize = new System.Drawing.Size(296, 360);
+           //this.Controls.Add(this.richTextBox1);
+            this.Menu = this.mainMenu1;
+            this.Text = "RTF Document Browser";
+            InitializeComponent();
+
+            DirectoryInfo dinfo = new DirectoryInfo(Path.GetDirectoryName(replayDirectory));
+            FileInfo[] Files = dinfo.GetFiles("*.rofl");
+
+            listView1.Columns.Add("File Name", -2);
+            listView1.Columns.Add("Date Created", -2);
+
+            foreach (FileInfo file in Files)
+            {
+                string[] details = new string[2];
+                details[0] = file.Name; details[1] = file.CreationTime.ToString("HH:mm dd/MM/yyyy");
+                ListViewItem item = new ListViewItem(details);
+                listView1.Items.Add(item);
+            }
+
+            listView1.MouseDoubleClick += new MouseEventHandler(listView1_MouseDoubleClick);
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ListViewHitTestInfo info = listView1.HitTest(e.X, e.Y);
+            ListViewItem item = info.Item;
+
+            if (item != null)
+            {
+                Process process = new Process();
+                string leaguePath = @"D:\Riot Games\League of Legends\RADS\solutions\lol_game_client_sln\releases\0.0.1.152\deploy\League of Legends.exe";
+                process.StartInfo.FileName = "League of Legends.exe";
+                process.StartInfo.WorkingDirectory = Path.GetDirectoryName(leaguePath);
+                process.StartInfo.Arguments = "\"League Of Legends.exe\" \"D:\\Users\\Navi Jador\\Documents\\League of Legends\\Replays\\" + item.Text;
+                process.Start();
+            }
+
+        }
+
+        // Bring up a dialog to open a file.
+        private void leagueDirectoryMenuItem_Click(object sender, System.EventArgs e)
+        {
+            // Show the FolderBrowserDialog.
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                leagueDirectory = folderBrowserDialog1.SelectedPath;
+                if (!fileOpened)
+                {
+                    leagueDirectoryMenuItem.PerformClick();
+                }
+            }
+        }
+
+        // Bring up a dialog to chose a folder path in which to open or save a file.
+        private void replayDirectoryMenuItem_Click(object sender, System.EventArgs e)
+        {
+            // Show the FolderBrowserDialog.
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                replayDirectory = folderBrowserDialog1.SelectedPath;
+                if (!fileOpened)
+                {
+                    leagueDirectoryMenuItem.PerformClick();
+                }
+            }
+        }
+    }
+}
