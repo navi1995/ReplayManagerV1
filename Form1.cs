bc9ccbb5acc2ccffc400c183e3182803a4fa3054
navi1995 <navi1995@hotmail.com>
2016-11-18 20:26:43 +1300

    Laptop progress merging

--- Form1.cs
+++ Form1.cs
@@ -14,14 +14,15 @@ namespace ReplayManagerv1
 {
     public partial class Form1 : Form
     {
-        private FolderBrowserDialog folderBrowserDialog1;
-        
-
+        private int counter = 0;
+        private OpenFileDialog leagueBrowserDialog;
+        private FolderBrowserDialog replayBrowserDialog;
         private MainMenu mainMenu1;
         private MenuItem fileMenuItem, leagueDirectoryMenuItem, replayDirectoryMenuItem;
 
         private string leagueDirectory = @"D:\Riot Games\League of Legends\RADS\solutions\lol_game_client_sln\releases\0.0.1.152\deploy\League of Legends.exe";
         private string replayDirectory = @"D:\Users\Navi Jador\Documents\League of Legends\Replays\";
+        private string username = "navi1995";
 
         private bool fileOpened = false;
 
@@ -31,8 +32,9 @@ namespace ReplayManagerv1
             this.fileMenuItem = new System.Windows.Forms.MenuItem();
             this.leagueDirectoryMenuItem = new System.Windows.Forms.MenuItem();
             this.replayDirectoryMenuItem = new System.Windows.Forms.MenuItem();
-            
-            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+
+            this.leagueBrowserDialog = new System.Windows.Forms.OpenFileDialog();
+            this.replayBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
 
             this.mainMenu1.MenuItems.Add(this.fileMenuItem);
             this.fileMenuItem.MenuItems.AddRange(
@@ -47,35 +49,46 @@ namespace ReplayManagerv1
             this.replayDirectoryMenuItem.Click += new System.EventHandler(this.replayDirectoryMenuItem_Click);
 
             // Set the help text description for the FolderBrowserDialog.
-            this.folderBrowserDialog1.Description =
-                "Select the directory that you want to use as the default.";
+            this.replayBrowserDialog.Description = "Select location of your replay (.rofl) files";
 
             // Do not allow the user to create new files via the FolderBrowserDialog.
-            this.folderBrowserDialog1.ShowNewFolderButton = false;
-
-            // Default to the My Documents folder.
+            this.replayBrowserDialog.ShowNewFolderButton = false;
+            this.leagueBrowserDialog.Filter = "League of Legends.exe (*.exe)|League Of Legends.exe";
 
             this.ClientSize = new System.Drawing.Size(296, 360);
-           //this.Controls.Add(this.richTextBox1);
+            //this.Controls.Add(this.richTextBox1);
             this.Menu = this.mainMenu1;
-            this.Text = "RTF Document Browser";
+            this.Text = "League of Legends replay manager";
             InitializeComponent();
+            refreshReplays();
+        }
 
-            DirectoryInfo dinfo = new DirectoryInfo(Path.GetDirectoryName(replayDirectory));
-            FileInfo[] Files = dinfo.GetFiles("*.rofl");
-
-            listView1.Columns.Add("File Name", -2);
-            listView1.Columns.Add("Date Created", -2);
-
-            foreach (FileInfo file in Files)
+        private void refreshReplays()
+        {
+            counter++;
+            try
             {
-                string[] details = new string[2];
-                details[0] = file.Name; details[1] = file.CreationTime.ToString("HH:mm dd/MM/yyyy");
-                ListViewItem item = new ListViewItem(details);
-                listView1.Items.Add(item);
+                DirectoryInfo dinfo = new DirectoryInfo(Path.GetDirectoryName(replayDirectory));
+                FileInfo[] Files = dinfo.GetFiles("*.rofl");
+                listView1.Clear();
+                listView1.Columns.Add("File Name", -2);
+                listView1.Columns.Add("Date Created", -2);
+
+                foreach (FileInfo file in Files)
+                {
+                    string[] details = new string[2];
+                    details[0] = file.Name; details[1] = file.CreationTime.ToString("HH:mm dd/MM/yyyy");
+                    ListViewItem item = new ListViewItem(details);
+                    listView1.Items.Add(item);
+                }
+
+                listView1.MouseDoubleClick += new MouseEventHandler(listView1_MouseDoubleClick);
+            }
+            catch (Exception e)
+            {
+                this.label1.Text = "File directories not correct, please check folder paths in settings!" + counter;
             }
 
-            listView1.MouseDoubleClick += new MouseEventHandler(listView1_MouseDoubleClick);
         }
 
         private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
@@ -99,14 +112,15 @@ namespace ReplayManagerv1
         private void leagueDirectoryMenuItem_Click(object sender, System.EventArgs e)
         {
             // Show the FolderBrowserDialog.
-            DialogResult result = folderBrowserDialog1.ShowDialog();
-            if (result == DialogResult.OK)
+            DialogResult result = leagueBrowserDialog.ShowDialog();
+            if (result == DialogResult.OK && Path.GetExtension(leagueBrowserDialog.FileName) == ".exe" && Path.GetFileName(leagueBrowserDialog.FileName) != "League of Legends")
             {
-                leagueDirectory = folderBrowserDialog1.SelectedPath;
-                if (!fileOpened)
-                {
-                    leagueDirectoryMenuItem.PerformClick();
-                }
+                //Implement STRICT checks that it's lol_game_client
+                leagueDirectory = leagueBrowserDialog.FileName;
+                refreshReplays();
+            } else
+            {
+                this.label1.Text = "Invalid file chosen, please find the League of Legends.exe file!";
             }
         }
 
@@ -114,14 +128,15 @@ namespace ReplayManagerv1
         private void replayDirectoryMenuItem_Click(object sender, System.EventArgs e)
         {
             // Show the FolderBrowserDialog.
-            DialogResult result = folderBrowserDialog1.ShowDialog();
-            if (result == DialogResult.OK)
+            DialogResult result = replayBrowserDialog.ShowDialog();
+            if (result == DialogResult.OK && Path.GetFileName(replayBrowserDialog.SelectedPath) == "Replays")
             {
-                replayDirectory = folderBrowserDialog1.SelectedPath;
-                if (!fileOpened)
-                {
-                    leagueDirectoryMenuItem.PerformClick();
-                }
+                //Implement REPLAY directory checks
+                replayDirectory = replayBrowserDialog.SelectedPath;
+                refreshReplays();
+            } else
+            {
+                this.label1.Text = "Invalid folder chosen, please choose the folder named \"Replays\"!";
             }
         }
     }
